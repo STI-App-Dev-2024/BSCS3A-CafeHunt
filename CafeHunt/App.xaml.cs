b9da6aaafbcba1994d2228b6f91e1ed907a37c7a
GelmoreLaguio <gmlaguio716@gmail.com>
2024-08-24 18:52:14 +0800

    Uploading Project File in Visual Studio for .Net Maui

---
+++ CafeHunt/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CafeHunt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CafeHunt/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CafeHunt
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
